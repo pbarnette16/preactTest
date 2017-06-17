dc43ed451dfe1583e1c4737a33d53c91e3a08aef
Paul Barnette <paul.barnette@gmail.com>
2017-06-17 16:34:56 +1000

    Update to enable push notifications

--- backend/setup.js
+++ backend/setup.js
@@ -1,4 +1,6 @@
-var cmd = require('node-cmd')
+const cmd = require('node-cmd')
+const webpush = require('web-push')
+const nano = require('nano')('http://localhost:5984')
 
 // Set couch running
 cmd.get('brew services start couchdb',
@@ -38,3 +40,56 @@ cmd.get('add-cors-to-couchdb',
     }
   }
 )
+
+// VAPID keys should only be generated only once.
+const vapidKeys = webpush.generateVAPIDKeys()
+if (vapidKeys.publicKey && vapidKeys.privateKey) {
+  nano.db.create('push-keys') // need to update this so that it doesnt keep creating the db
+  var pushKeys = nano.db.use('push-keys')
+  pushKeys.list(function (err, body) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('List all the keys len ' + body.rows.length)
+
+      // we dont have a key put it in the DB
+      if (body.rows.length === 0) {
+        pushKeys.insert(vapidKeys, null, function (err, body) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log('Added the pushKeys to the couchdb')
+            setVapidDetails(vapidKeys)
+            console.log('Push notifications set up')
+          }
+        })
+      } else {
+        getRowData(body.rows[0].id).then(function (data) {
+          setVapidDetails(data)
+          console.log('Push notifications set up')
+        })
+      }
+      // console.log('localKey')
+      // console.log(localKey)
+    }
+  })
+}
+
+var getRowData = function (id) {
+  var pushKeys = nano.db.use('push-keys')
+  return new Promise(function (resolve, reject) {
+    pushKeys.get(id, function (err, body) {
+      if (!err) {
+        return resolve(body)
+      }
+    })
+  })
+}
+
+var setVapidDetails = function (keys) {
+  webpush.setVapidDetails(
+  'mailto:paul.barnette@gmail.com',
+  keys.publicKey,
+  keys.privateKey
+)
+}
